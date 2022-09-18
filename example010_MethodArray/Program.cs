aa630d57023559760b13bde267a1910ba2f2d096
anna.msklv <anna.msklv@yandex.ru>
2022-09-18 12:21:15 +0300

    example010_MethodArray

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {1, 12, 31, 4, 18, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;  // для того, чтобы если в массиве два одинаковых числа, отобразилась только позиция первого числа
+    }
+    index++;
+}
